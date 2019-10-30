560c9afcfe758c7f34f9ecd257a91fcbb71d26b4
Idowu <iarifayan@russelsmith.local>
2019-10-30 09:59:31 +0100

    Changes made

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ function Home({ products }) {
 
 Home.getInitialProps = async () => {
   const url = "http://localhost:3000/api/products";
-  const res = await axios.get(url);
+  const res = await axios.get(url, { withCredentials: true });
   return { products: res.data };
 };
 
