06b635eca3295197b7a984d06a120f23a3a7ea2f
Idowu <iarifayan@russelsmith.local>
2019-10-30 10:50:40 +0100

    Updated next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "<insert-mongo-srv>",
+    MONGO_SRV:
+      "mongodb+srv://stizzle:bossman2@cluster0-8uqji.mongodb.net/reactreserve?retryWrites=true&w=majority",
     JWT_SECRET: "<insert-jwt-secret>",
     CLOUDINARY_URL: "<insert-cloudinary-url>",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
